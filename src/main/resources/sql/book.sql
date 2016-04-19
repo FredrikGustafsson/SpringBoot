0df9b5f5d95bd2dd1472f8af646462fe433069a7
n259970 <fredrik.gustafsson@nordea.com>
2016-04-19 07:41:11 +0200

    added sql file

---
+++ src/main/resources/sql/book.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `book` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `IPUSER` varchar(45) DEFAULT NULL,
+  `timestamp` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
